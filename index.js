e9e75605700f68d94934037aa14b38cce0f33f93
SANDRA CALZADA <127830026+sndr157@users.noreply.github.com>
2023-10-31 13:18:13 +0100

    added new modules

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import express from 'express'
 import getBooks from './services/books/getBooks.js'
 import getBookById from './services/books/getBookById.js'
+import updateBookById from './services/books/updateBookById.js'
 import createBook from './services/books/createBook.js'
+import deleteBook from './services/books/deleteBook.js'
 
 const app = express()
 
+// Middleware to parse JSON request bodies
+app.use(express.json())
+
+// Root route to test if the server is running
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Route to get a list of books
 app.get('/books', (req, res) => {
   try {
     const books = getBooks()
     res.status(200).json(books)
   } catch (error) {
     console.error(error)
-    res.status(500).send('Something went wrong while getting list of books!')
+    res.status(500).send('Something went wrong while getting the list of books!')
   }
 })
 
+// Route to get a specific book by ID
 app.get('/books/:id', (req, res) => {
   try {
     const { id } = req.params
@@ -31,21 +39,43 @@ app.get('/books/:id', (req, res) => {
     }
   } catch (error) {
     console.error(error)
-    res.status(500).send('Something went wrong while getting book by id!')
+    res.status(500).send('Something went wrong while getting the book by id!')
   }
 })
 
-app.post('/books', (req, res) => {
+// Route to update a book by ID
+app.put('/books/:id', (req, res) => {
   try {
+    const { id } = req.params
     const { title, author, isbn, pages, available, genre } = req.body
-    const newBook = createBook(title, author, isbn, pages, available, genre)
-    res.status(201).json(newBook)
+    const updatedBook = updateBookById(id, title, author, isbn, pages, available, genre)
+    res.status(200).json(updatedBook)
   } catch (error) {
     console.error(error)
-    res.status(500).send('Something went wrong while creating new book!')
+    res.status(500).send('Something went wrong while updating the book by id!')
   }
 })
 
+// Route to Delete a book by ID
+app.delete('/books/:id', (req, res) => {
+  try {
+    const { id } = req.params
+    const deletedBookId = deleteBook(id)
+
+    if (!deletedBookId) {
+      res.status(404).send(`Book with id ${id} was not found!`)
+    } else {
+      res.status(200).json({
+        message: `Book with id ${deletedBookId} was deleted!`
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Something went wrong while deleting book by id!')
+  }
+})
+
+// Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
 })
