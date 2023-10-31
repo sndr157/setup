65dcc2e7285f3c59caa90475403ecb5a2b297036
SANDRA CALZADA <127830026+sndr157@users.noreply.github.com>
2023-10-31 11:01:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import getBooks from './services/books/getBooks.js'
+import getBookById from './services/books/getBookById.js'
 
 const app = express()
 
@@ -14,6 +15,21 @@ app.get('/books', (req, res) => {
   } catch (error) {
     console.error(error)
     res.status(500).send('Something went wrong while getting list of books!')
+  }
+})
+app.get('/books/:id', (req, res) => {
+  try {
+    const { id } = req.params
+    const book = getBookById(id)
+
+    if (!book) {
+      res.status(404).send(`Book with id ${id} was not found!`)
+    } else {
+      res.status(200).json(book)
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Something went wrong while getting book by id!')
   }
 })
 
