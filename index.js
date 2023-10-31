2ecb6f09ff16f760c1bb09e414b5f95bd3605049
SANDRA CALZADA <127830026+sndr157@users.noreply.github.com>
2023-10-31 14:52:11 +0100

    updated p.j

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ import express from 'express'
 import booksRouter from './routes/books.js'
 
 const app = express()
-
-// Middleware to parse JSON request bodies
 app.use(express.json())
 
-app.us('/books', booksRouter)
+app.use('/books', booksRouter)
 
-// Root route to test if the server is running
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
 })
