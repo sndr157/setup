c0bd60019ce075923b4276668ad52cfc9e88b33e
SANDRA CALZADA <127830026+sndr157@users.noreply.github.com>
2023-11-03 10:44:14 +0100

    updated npm and add missing dependencies

--- Middleware/advancedAuth.js
+++ Middleware/advancedAuth.js
@@ -1,8 +1,8 @@
 import { auth } from 'express-oauth2-jwt-bearer'
 
 const checkJwt = auth({
-  audience: --'https://book-store-api', // e.g. https://book-store-api
-  issuerBaseURL: `https://{yourDomain}/`
+  audience: '{yourApiIdentifier}', // e.g. https://book-store-api
+  issuerBaseURL: 'https://{yourDomain}/'
 })
 
 export default checkJwt
